84f37d3eb97a8ffa3f484da08eecf5c80c5d6b3c
vikingJason <51340656+vikingJason@users.noreply.github.com>
2024-07-09 07:11:10 +1000

    Quartz sync: Jul 9, 2024, 7:11 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-AU",
+    locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
