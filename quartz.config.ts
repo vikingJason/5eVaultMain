931a29f7f72410269b725f3d5f45450b540049cb
vikingJason <51340656+vikingJason@users.noreply.github.com>
2024-07-23 08:15:22 +1000

    Quartz sync: Jul 23, 2024, 8:15 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "content/assets"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
