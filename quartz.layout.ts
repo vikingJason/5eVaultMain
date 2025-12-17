786db72d44b6be6ad2d3420f4646e5be85d24fde
vikingJason <51340656+vikingJason@users.noreply.github.com>
2025-12-17 21:12:48 +1100

    Quartz sync: Dec 17, 2025, 9:12 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,7 +4,9 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [],
+  header: [
+    Component.AnimatedDice(),
+  ],
   afterBody: [],
   footer: Component.Footer({
     links: {
