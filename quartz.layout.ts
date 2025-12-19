2b13962294454da0c4a9f8307b61f2c0935b3e9a
vikingJason <51340656+vikingJason@users.noreply.github.com>
2025-12-19 19:47:56 +1100

    Quartz sync: Dec 19, 2025, 7:47 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,9 +4,7 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [
-    Component.DiceRollerFixed(),
-  ],
+  header: [],
   afterBody: [],
   footer: Component.Footer({
     links: {
--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -20,7 +20,6 @@ import MobileOnly from "./MobileOnly"
 import RecentNotes from "./RecentNotes"
 import Breadcrumbs from "./Breadcrumbs"
 import Comments from "./Comments"
-import DiceRollerFixed from "./DiceRollerFixed"
 
 export {
   ArticleTitle,
@@ -45,5 +44,4 @@ export {
   NotFound,
   Breadcrumbs,
   Comments,
-  DiceRollerFixed,
 }
