484c2b055d4e9927bfd334a1f1f907f8f13b7920
vikingJason <51340656+vikingJason@users.noreply.github.com>
2026-01-06 17:51:09 +1100

    Quartz sync: Jan 6, 2026, 5:51 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -100,10 +100,10 @@ const config: QuartzConfig = {
       }),
       Plugin.Assets(),
       Plugin.Static(),
-      // Plugin.Favicon("static/icon.png"),
+      Plugin.Favicon(),
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
-      // Plugin.CustomOgImages(),
+      Plugin.CustomOgImages(),
     ],
   },
 }
